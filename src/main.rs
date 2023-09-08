ddec5805a5c4c33cabb6d8d9c63973095f00c792
James Cuzella <james.cuzella@lyraphase.com>
2023-09-08 16:31:51 -0600

    Example: Creating Instances from Other Instances w/Struct Update Syntax

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ fn main() {
 
     // Using the Field Init Shorthand
     using_field_init_shorthand();
+
+    // Creating Instances from Other Instances with Struct Update Syntax
+    struct_update_syntax();
 }
 
 // Example: Defining and Instantiating Structs
@@ -65,4 +68,35 @@ fn using_field_init_shorthand() {
     println!("`user2` is: {:#?}", user2);
 }
 
+// Example: Creating Instances from Other Instances with Struct Update Syntax
+fn struct_update_syntax() {
+    println!("struct_update_syntax():");
+    let user1 = User {
+        active: true,
+        username: String::from("brubble"),
+        email: String::from("barney.rubble@slate-rock-n-gravel.com"),
+        sign_in_count: 1,
+    };
+    println!("`user1` is: {:#?}", user1);
+    let user2 = User {
+        active: user1.active,
+        username: user1.username.clone(),
+        email: String::from("another@example.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+    println!("`user2` is: {:#?}", user2);
+    println!("`user1` is still: {:#?}", user1); // No compile error b/c String fields were cloned
 
+    // Shorthand Struct Update Syntax
+    let user3 = User {
+        email: String::from("struct-update@example.com"),
+        ..user1 // = just use all other values from user1
+        // FYI! b/c String does NOT implement Copy trait,
+        // String fields from user1 are __moved__ into user3's fields
+    };
+    println!("`user3` is: {:#?}", user3);
+
+    // Because user1's String fields were moved, the following won't compile
+    // println!("`user1` is now unusable: {:#?}", user1); // Compile Error: borrow of partially moved value: `user1`
+    // partial move occurs because `user1.username` has type `String`, which does not implement the `Copy` trait
+}
