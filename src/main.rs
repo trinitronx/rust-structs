76eae53f687721b9543419d7c08bbcd53b24553e
James Cuzella <james.cuzella@lyraphase.com>
2023-09-08 15:29:02 -0600

    Example: Using the Field Init Shorthand

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 fn main() {
     // Defining and Instantiating Structs
     defining_and_instantiating_structs();
+
+    // Using the Field Init Shorthand
+    using_field_init_shorthand();
 }
 
 // Example: Defining and Instantiating Structs
@@ -27,3 +30,39 @@ fn defining_and_instantiating_structs() {
         user1.active, user1.username, user1.email, user1.sign_in_count
     );
 }
+
+// Example: Using the Field Init Shorthand
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username: username,
+        email: email,
+        sign_in_count: 1,
+    }
+}
+
+// Field names are the same as struct attribute names
+// So, shorthand notation is allowed
+fn build_user_shorthand(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+fn using_field_init_shorthand() {
+    println!("using_field_init_shorthand():");
+    let user1 = build_user(
+        "barney.rubble@slate-rock-n-gravel.com".to_string(),
+        "brubble".to_string(),
+    );
+    println!("`user1` is: {:#?}", user1);
+    let user2 = build_user_shorthand(
+        "fred.flintstone@slate-rock-n-gravel.com".to_string(),
+        "fflintstone".to_string(),
+    );
+    println!("`user2` is: {:#?}", user2);
+}
+
+
