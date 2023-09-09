f896d6d3f757925a110ee2ef3c7cd5450f2e35b3
James Cuzella <james.cuzella@lyraphase.com>
2023-09-09 10:08:21 -0600

    Add all missing docstrings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/*! # Rust Book Ch. 5: Using Structs to Structure Related Data
+    
+    
+    A struct, or structure, is a custom data type that lets you package
+    together and name multiple related values that make up a meaningful group.
+    If you’re familiar with an object-oriented language, a struct is like an
+    object’s data attributes. In this chapter, we’ll compare and contrast
+    tuples with structs to build on what you already know and demonstrate when
+    structs are a better way to group data.
+    
+    We’ll demonstrate how to define and instantiate structs. We’ll discuss how
+    to define associated functions, especially the kind of associated functions
+    called methods, to specify behavior associated with a struct type.
+    Structs and enums (discussed in Chapter 6) are the building blocks for
+    creating new types in your program’s domain to take full advantage of
+    Rust’s compile-time type checking.
+*/
+
+/// # `main()` function to demonstrate examples from Rust Book Ch. 5
 fn main() {
     // Defining and Instantiating Structs
     defining_and_instantiating_structs();
@@ -30,7 +49,8 @@ struct User {
     sign_in_count: u64,
 }
 
-/// Defining and Instantiating Structs
+/// # Defining and Instantiating Structs
+///
 /// Structs are similar to tuples, discussed in “The Tuple Type” section,
 /// in that both hold multiple related values. Like tuples, the pieces
 /// of a struct can be different types. Unlike with tuples,
@@ -54,7 +74,10 @@ fn defining_and_instantiating_structs() {
     );
 }
 
-// Example: Using the Field Init Shorthand
+/// # Example: Using the Field Init Shorthand
+/// 
+/// A `build_user` function that returns a `User` instance with the given email
+/// and username. Uses owned `String` types to store `username` and `email`.
 fn build_user(email: String, username: String) -> User {
     User {
         active: true,
@@ -64,8 +87,9 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
-// Field names are the same as struct attribute names
-// So, shorthand notation is allowed
+/// # Using the Field Init Shorthand
+/// Field names are the same as struct attribute names
+/// So, shorthand notation is allowed
 fn build_user_shorthand(email: String, username: String) -> User {
     User {
         active: true,
@@ -74,6 +98,13 @@ fn build_user_shorthand(email: String, username: String) -> User {
         sign_in_count: 1,
     }
 }
+
+/// # Using the Field Init Shorthand
+/// 
+/// Because the parameter names and the struct field names are exactly the same
+/// in `build_user()`, we can use the field init shorthand syntax to
+/// rewrite `build_user` so it behaves exactly the same but doesn’t have the
+/// repetition of `username` and `email`, as shown `build_user_shorthand()`.
 fn using_field_init_shorthand() {
     println!("using_field_init_shorthand():");
     let user1 = build_user(
@@ -88,12 +119,12 @@ fn using_field_init_shorthand() {
     println!("`user2` is: {:#?}", user2);
 }
 
-/// Example: Creating Instances from Other Instances with Struct Update Syntax
+/// # Example: Creating Instances from Other Instances with Struct Update Syntax
 /// 
 /// It’s often useful to create a new instance of a struct that includes most
 /// of the values from another instance, but changes some.
 /// You can do this using struct update syntax.
-/// First, in this function we show how to create a new User instance in user2
+/// First, in this function we show how to create a new User instance in `user2`
 /// regularly, without the update syntax. We set a new value for email but
 /// otherwise use the same values from `user1` that we created in
 /// `using_field_init_shorthand()`.
@@ -129,21 +160,24 @@ fn struct_update_syntax() {
     // partial move occurs because `user1.username` has type `String`, which does not implement the `Copy` trait
 }
 
-// Example: Using Tuple Structs Without Named Fields to Create Different Types
+/// # Example: Using Tuple Structs Without Named Fields to Create Different Types
+/// 
 /// A Tuple Struct for representing an RGB color,
-/// composed of 3x i32 numbers
+/// composed of 3x `i32` numbers
 #[derive(Debug, Copy, Clone)]
 struct Color(i32, i32, i32);
-/// A Tuple Struct for representing an R3 Point,
-/// composed of 3x i32 numbers for X, Y, and Z coords
+/// A Tuple Struct for representing an ℝ3 `Point`,
+/// composed of 3x `i32` numbers for `X`, `Y`, and `Z` coords
 #[derive(Debug)]
 struct Point(i32, i32, i32);
+/// # Using Tuple Structs Without Named Fields to Create Different Types
+/// 
 /// Rust also supports structs that look similar to tuples, called tuple structs.
 /// Tuple structs have the added meaning the struct name provides but don’t
 /// have names associated with their fields;
 /// rather, they just have the types of the fields.
 ///
-/// When to use Tuple Structs
+/// ## When to use Tuple Structs
 /// Tuple structs are useful when you want to give the whole tuple a name and
 /// make the tuple a different type from other tuples, and when naming each
 /// field as in a regular struct would be verbose or redundant.
@@ -165,11 +199,13 @@ fn tuple_structs() {
     // expected `Color`, found `Point`
 }
 
+/// # Tuple Structs Are Unique Types
+/// 
 /// Each struct you define is its own type,
 /// even though the fields within the struct might have the same types.
-/// For example, a function that takes a parameter of type Color
-/// cannot take a Point as an argument,
-/// even though both types are made up of three i32 values.
+/// For example, a function that takes a parameter of type `Color`
+/// cannot take a `Point` as an argument,
+/// even though both types are made up of three `i32` values.
 fn tuple_structs_are_unique_types(c: &mut Color) -> &mut Color {
     // tuple struct instances are similar to tuples
     // you can destructure them into their individual pieces,
@@ -180,13 +216,15 @@ fn tuple_structs_are_unique_types(c: &mut Color) -> &mut Color {
     c
 }
 
-// Example: Unit-Like Structs Without Any Fields
-/// An example Unit-Like Struct called: "AlwaysEqual"
+/// # Example: Unit-Like Structs Without Any Fields
+/// An example Unit-Like Struct called: "`AlwaysEqual`"
 #[derive(Debug)]
 struct AlwaysEqual;
 
+/// # Unit-Like Structs Without Any Fields
+/// 
 /// You can also define structs that don’t have any fields!
-/// These are called unit-like structs because they behave similarly to (),
+/// These are called unit-like structs because they behave similarly to `()`,
 /// the unit type
 /// 
 /// When to Use Unit-Like Structs
