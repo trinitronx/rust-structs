ea5443475d42ec07f4692ed5284354067c84e596
James Cuzella <james.cuzella@lyraphase.com>
2023-09-09 09:56:08 -0600

    Add docs for section: Defining and Instantiating Structs

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,14 @@ struct User {
     sign_in_count: u64,
 }
 
+/// Defining and Instantiating Structs
+/// Structs are similar to tuples, discussed in “The Tuple Type” section,
+/// in that both hold multiple related values. Like tuples, the pieces
+/// of a struct can be different types. Unlike with tuples,
+/// in a struct you’ll name each piece of data so it’s clear what the values
+/// mean. Adding these names means that structs are more flexible than tuples:
+/// you don’t have to rely on the order of the data to specify or access the
+/// values of an instance.
 fn defining_and_instantiating_structs() {
     println!("defining_and_instantiating_structs()");
     let mut user1 = User {
